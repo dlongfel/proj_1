84881906a62be13bf5278f56c74f9d58ed192cde
Student <student@mirea.ru>
2021-02-11 10:52:31 +0300

    1st

---
+++ src/test/java/com/mylabs/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mylabs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
